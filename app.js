90de56e998b248ab2945745dc7af7074dbe49ad2
dfoltynski <dawidfoltynski7@gmail.com>
2026-02-28 20:08:45 +0100

    feat: update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get("/", (req, res) => {
     console.log(req.query);
-    res.json({txt: "hello test branch"})
+    res.json({txt: "hello test branch 2"})
 })
 
 app.listen(8181, () => {
